5bc9e657b3849a0cf55796e6364161a0216648c2
Grenish rai <mrcoder2033d@gmail.com>
2025-10-23 23:44:51 +0530

    updated theme add tweakcn script for testing.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="w-full min-h-screen flex items-center justify-center">
-      <h2>Hello World</h2>
+      <h2 className="text-foreground">Hello World</h2>
     </div>
   );
 }
