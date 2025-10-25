a05e2ee2ad2608e143acbfbe4eebf635ffe47f09
Grenish rai <mrcoder2033d@gmail.com>
2025-10-25 20:40:47 +0530

    Use Next Image for logos and background
    Replace lucide icon with Next.js Image logo and swap background img for
    a responsive Next Image. Update brand text to "FuelDev" and remove the
    unused GalleryVerticalEnd import.

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,8 +1,7 @@
-import { GalleryVerticalEnd } from "lucide-react";
 import { Suspense } from "react";
-
 import { LoginForm } from "@/components/login-form";
 import { Spinner } from "@/components/ui/spinner";
+import Image from "next/image";
 
 function LoginFormSuspense() {
   return (
@@ -23,11 +22,14 @@ export default function LoginPage() {
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-              <GalleryVerticalEnd className="size-4" />
-            </div>
-            Acme Inc.
+          <a href="#" className="flex items-center gap-1 font-medium">
+            <Image
+              src={"/logo-min.png"}
+              alt="FuelDev Logo"
+              width={40}
+              height={40}
+            />
+            FuelDev
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
@@ -37,11 +39,7 @@ export default function LoginPage() {
         </div>
       </div>
       <div className="bg-muted relative hidden lg:block">
-        <img
-          src="/placeholder.svg"
-          alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
+        <Image src="/milky-way.png" alt="Milky Way Background" fill priority />
       </div>
     </div>
   );
