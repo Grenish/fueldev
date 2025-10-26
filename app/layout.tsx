946c4898138572bb50549d27ab06aad2f22c9f14
Grenish rai <mrcoder2033d@gmail.com>
2025-10-26 22:57:53 +0530

    Add Sonner Toaster to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { TRPCProvider } from "@/lib/trpc/react";
+import { Toaster } from "sonner";
 
 const inter = Inter({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -30,6 +31,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <TRPCProvider>{children}</TRPCProvider>
+          <Toaster richColors position="top-right" />
         </ThemeProvider>
       </body>
     </html>
