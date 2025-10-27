be4f3c4e429973aab6f952e7a8bf65bd2e4155a2
Grenish rai <mrcoder2033d@gmail.com>
2025-10-27 16:58:57 +0530

    Add 'Add New Link' button to Creator Hub

--- app/(after-auth)/(creators)/hub/links/page.tsx
+++ app/(after-auth)/(creators)/hub/links/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+
 export default function CreatorLinks() {
   return (
-    <div>
-      <h2>Hello Link</h2>
+    <div className="flex flex-col items-center justify-center p-2">
+      <Button className="bg-secondary" variant={"default"} size={"sm"}>
+        <Plus className="" size={20} /> <p>Add New Link</p>
+      </Button>
     </div>
   );
 }
