dc78f9e9bba4bef35c90b4953e690a7f7d327cdd
Grenish rai <mrcoder2033d@gmail.com>
2025-10-27 16:59:28 +0530

    Require auth session and use dynamic breadcrumb

--- app/(after-auth)/layout.tsx
+++ app/(after-auth)/layout.tsx
@@ -1,51 +1,50 @@
 import { AppSidebar } from "@/components/protected/app-sidebar";
+import { DynamicBreadcrumb } from "@/components/protected/dynamic-breadcrumb";
 import { ThemeSwitcher } from "@/components/theme-switcher";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
 import { Separator } from "@/components/ui/separator";
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function protectedLayout({
+export default async function protectedLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+    query: {
+      disableCookieCache: true,
+    },
+  });
+
+  if (!session) {
+    redirect("/auth/login");
+  }
+
+  const { user } = session;
+
   return (
     <SidebarProvider>
-      <AppSidebar />
+      <AppSidebar user={user} />
       <SidebarInset>
-        <main className="w-full bg-muted/50 h-[calc(100svh-1rem)] rounded-none sm:rounded-2xl flex flex-col overflow-hidden">
-          <header className="flex h-16 shrink-0 items-center justify-between px-4 border-b">
+        <main className="relative w-full bg-muted/50 h-[calc(100svh-1rem)] rounded-none sm:rounded-2xl overflow-hidden">
+          <header className="absolute top-0 left-0 right-0 z-10 flex h-16 shrink-0 items-center justify-between px-4 backdrop-blur-sm  ">
             <div className="flex items-center gap-2">
               <SidebarTrigger className="-ml-1" />
               <Separator
                 orientation="vertical"
                 className="mr-2 data-[orientation=vertical]:h-4"
               />
-              <Breadcrumb>
-                <BreadcrumbList>
-                  <BreadcrumbItem className="hidden md:block">
-                    <BreadcrumbLink>Home</BreadcrumbLink>
-                  </BreadcrumbItem>
-                  <BreadcrumbSeparator className="hidden md:block" />
-                  <BreadcrumbItem>
-                    <BreadcrumbPage>Grenish Rai</BreadcrumbPage>
-                  </BreadcrumbItem>
-                </BreadcrumbList>
-              </Breadcrumb>
+              <DynamicBreadcrumb userName={user.name} />
             </div>
             <ThemeSwitcher />
           </header>
 
-          <div className="flex-1 overflow-y-auto">{children}</div>
+          <div className="h-full overflow-y-auto pt-16">{children}</div>
         </main>
       </SidebarInset>
     </SidebarProvider>
