463f09a4cb502080cf4ffdb4200136c7743874a7
Grenish rai <mrcoder2033d@gmail.com>
2025-10-29 22:22:48 +0530

    Add placeholder creators hub posts page

---
+++ app/(after-auth)/(creators)/hub/posts/page.tsx
@@ -0,0 +1,10 @@
+import { Spinner } from "@/components/ui/spinner";
+
+export default function CreatorPage() {
+  return (
+    <div className="h-full w-full flex items-center justify-center gap-2">
+      <Spinner />
+      <h2>We&apos;r working on it. Hold on!</h2>
+    </div>
+  );
+}
