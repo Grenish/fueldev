4e9ec2189e8fd99990d8ee36b00dda468fddcfac
Grenish rai <mrcoder2033d@gmail.com>
2025-10-31 22:34:08 +0530

    Add CreatorArticle page component

---
+++ app/(after-auth)/(creators)/hub/posts/new/article/page.tsx
@@ -0,0 +1,10 @@
+export default function CreatorArticle() {
+  return (
+    <div className="h-full w-full flex flex-col items-center">
+      <div className="w-1/2 mx-auto p-5">
+        <h1 className="text-3xl font-semibold">Some Great Title</h1>
+        <p className="text-lg text-muted-foreground">some great subtitles</p>
+      </div>
+    </div>
+  );
+}
