8026c768dfa78c9338004e18fbd3d1186fb40f84
Grenish rai <mrcoder2033d@gmail.com>
2025-11-08 15:02:18 +0530

    Add articleEngagement router

--- server/routers/_app.ts
+++ server/routers/_app.ts
@@ -2,11 +2,13 @@ import { router } from "../trpc";
 import { userRouter } from "./user";
 import { userLinksRouter } from "./userLinks";
 import { articleRouter } from "./article";
+import { articleEngagementRouter } from "./articleEngagement";
 
 export const appRouter = router({
   user: userRouter,
   userLinks: userLinksRouter,
   article: articleRouter,
+  articleEngagement: articleEngagementRouter,
 });
 
 export type AppRouter = typeof appRouter;
