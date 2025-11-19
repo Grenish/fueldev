f477be97c395f284f38c7326fcb5600ddd22d830
Grenish rai <mrcoder2033d@gmail.com>
2025-11-19 23:16:05 +0530

    Register store router in appRouter

--- server/routers/_app.ts
+++ server/routers/_app.ts
@@ -3,12 +3,14 @@ import { userRouter } from "./user";
 import { userLinksRouter } from "./userLinks";
 import { articleRouter } from "./article";
 import { articleEngagementRouter } from "./articleEngagement";
+import { storeRouter } from "./store";
 
 export const appRouter = router({
   user: userRouter,
   userLinks: userLinksRouter,
   article: articleRouter,
   articleEngagement: articleEngagementRouter,
+  store: storeRouter,
 });
 
 export type AppRouter = typeof appRouter;
